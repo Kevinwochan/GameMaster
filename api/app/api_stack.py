daf97db088788c241017aabfd5fc0ab6ef0e31a5
kevinwochan <kevinwochan@gmail.com>
2021-08-22 01:28:50 +1000

    working API endpoint

--- api/app/api_stack.py
+++ api/app/api_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import core as cdk
-from aws_cdk import aws_lambda as _lambda, aws_apigateway as apigw
+from aws_cdk import aws_lambda as _lambda, aws_apigateway as apigw, aws_dynamodb as ddb
 
 
 class ApiStack(cdk.Stack):
@@ -7,15 +7,26 @@ class ApiStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        
+        table = ddb.Table(
+            self, 'Characters',
+            partition_key={'name': 'path', 'type': ddb.AttributeType.STRING}
+        )
+
         my_lambda = _lambda.Function(
             self, 'LambdaAPI',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('assets'),
             handler='lambda.handler',
+            environment={
+                'CHARACTER_TABLE': table.table_name
+            }
         )
 
         apigw.LambdaRestApi(
             self, 'Endpoint',
             handler=my_lambda,
         )
+
+        table.grant_read_write_data(my_lambda)
 
--- api/assets/lambda.py
+++ api/assets/lambda.py
@@ -1,12 +1,17 @@
 import json
+import boto3
+import os
 
+ddb = boto3.resource('dynamodb')
+table = ddb.Table(os.environ['CHARACTER_TABLE'])
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
+    response = table.scan(Select='ALL_ATTRIBUTES')
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
+        'body': json.dumps(response['Items'])
     }
