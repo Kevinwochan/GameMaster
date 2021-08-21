03f6d44225291ac82016c4766a2511983f232786
kevinwochan <kevinwochan@gmail.com>
2021-08-21 22:53:28 +1000

    beginning conversion to cdk

--- api/app.py
+++ api/app.py
@@ -1,55 +1,34 @@
-from chalice import Chalice, Response
-import boto3
-import json
+#!/usr/bin/env python3
+import os
 
-app = Chalice(app_name='GameMasterAPI')
+from aws_cdk import core as cdk
 
+# For consistency with TypeScript code, `cdk` is the preferred import name for
+# the CDK's core module.  The following line also imports it as `core` for use
+# with examples from the CDK Developer's Guide, which are in the process of
+# being updated to use `cdk`.  You may delete this import if you don't need it.
+from aws_cdk import core
 
-@app.route('/test')
-def index():
-    return Response(status_code=200,
-                    body=json.dumps({'Message': 'Hello World'}))
-
-
-@app.route('/')
-def characters():
-    '''
-    Fetches all characters in the DB
-    TODO: set max possible returned chars to 100
-    '''
-    try:
-        table = boto3.resource('dynamodb').Table('GameMasterCharacters')
-    except Exception as e:
-        print(e)
-        return Response(status_code=500,
-                        body=json.dumps({'Error': 'Server Error'}))
-    try:
-        response = table.scan(Select='ALL_ATTRIBUTES')
-        print('here we are')
-        print(response)
-        return Response(status_code=200, body=json.dumps(reponse['Items']))
-    except Exception as e:
-        print(e)
-        return Response(status_code=500,
-                        body=json.dumps({'Error': 'Server Error'}))
+from api.api_stack import ApiStack
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+app = core.App()
+ApiStack(app, "ApiStack",
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    #env=core.Environment(account='123456789012', region='us-east-1'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
+
+app.synth()
